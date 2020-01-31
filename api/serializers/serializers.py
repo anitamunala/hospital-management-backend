7c31b68c6fa21340b55f887678926b1cf4c3a5b2
lwairore <kwairore@gmail.com>
2020-01-31 13:38:40 +0300

    Rename NavBarItemsSerializer, and ServicesSerializer to NavBarItemSerializer and ServiceSerializer respectively.

--- api/serializers/serializers.py
+++ api/serializers/serializers.py
@@ -1,12 +1,12 @@
 from api.models import NavBarItems, Service, Doctor
 from rest_framework import serializers
 
-class NavBarItemsSerializer(serializers.ModelSerializer):
+class NavBarItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = NavBarItems
         fields = ['name']
 
-class ServicesSerializer(serializers.HyperlinkedModelSerializer):
+class ServiceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Service
         fields = ['name','description','image']

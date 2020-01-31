6424c601457460e90d473f49495b6d9db0ee9a87
lwairore <kwairore@gmail.com>
2020-01-31 11:24:08 +0300

    Import admin from 'django.contrib', and import models from api.
    Register NavBarItems,Doctor,Specialist,Patient and Service models by passing them as arguments to the register, 'admin.site', method.

---
+++ api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from api import models
+
+# Register your models here.
+admin.site.register(models.NavBarItems)
+admin.site.register(models.Doctor)
+admin.site.register(models.Specialist)
+admin.site.register(models.Patient)
+admin.site.register(models.Service)

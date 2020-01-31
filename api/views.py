7d04c937fa75ee757420181610244e88b4db875d
lwairore <kwairore@gmail.com>
2020-01-31 15:37:37 +0300

    Change ServicesViewSet to extend to APIView instead of ReadOnlyModelViewSet. Remove queryset and serializer_class variables. Define a get method that has self and request parameters respectively that fetches all items of Service model and uses Service serializer and returns the serialized data.

--- api/views.py
+++ api/views.py
@@ -20,12 +20,18 @@ class NavBarItemsViewSet(APIView):
         })
 
 
-class ServicesViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Service.objects.all()
-    serializer_class = ServicesSerializer
+class ServicesViewSet(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    def get(self, request):
+        all_services = models.Service.objects.all()
+        serialize_all_services = serializers.Service(all_services, many = True)
+        return Response({
+            'status':status.HTTP_200_OK,
+            'all_services':serialize_all_services.data
+            
+        })
 
-
-@api_view(['GET'])
+@api_view['GET'])
 def all_doctors(request):
     all_doctors_items = Doctor.objects.all()
     processed_doctors = []

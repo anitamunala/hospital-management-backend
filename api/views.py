371be51992f89f6362b73d597907ea1db71f8623
lwairore <kwairore@gmail.com>
2020-01-31 16:19:17 +0300

    Change all_doctors from being a function-based view to a class-based view accordingly.

--- api/views.py
+++ api/views.py
@@ -22,33 +22,27 @@ class NavBarItemsViewSet(APIView):
 
 class ServicesViewSet(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
     def get(self, request):
         all_services = models.Service.objects.all()
         serialize_all_services = serializers.ServiceSerializer(all_services, many = True)
         return Response({
             'status':status.HTTP_200_OK,
             'all_services':serialize_all_services.data
-            
         })
 
-@api_view['GET'])
-def all_doctors(request):
-    all_doctors_items = Doctor.objects.all()
-    processed_doctors = []
-    for doctor in all_doctors_items:
-        temporary_doctor = {
-            'name': doctor.name,
-            'profile_picture': f'{doctor.profile_picture}',
-            'specialization': doctor.specialization.name,
 
-        }
-        processed_doctors.append(temporary_doctor)
-    print(processed_doctors)
-    return Response({
-        'status':  HTTP_200_OK,
-        'data': processed_doctors
-
-    })
+class  AllDoctors(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    
+    def get(self, request):
+        all_doctors = models.Doctor.objects.all()
+        serialize_all_doctors = serializers.DoctorSerializer(all_doctors, many= True)
+        return Response({
+            'status' : status.HTTP_200_OK,
+            'all_doctors': serialize_all_doctors.data
+        })
+    
     
 
 

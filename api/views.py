90e0da453c0640259e40345b26588bd6a09d1adc
lwairore <kwairore@gmail.com>
2020-01-31 15:47:31 +0300

    alter the serializer class of ServicesViewSet class to ServiceSerializer from Service.

--- api/views.py
+++ api/views.py
@@ -24,7 +24,7 @@ class ServicesViewSet(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     def get(self, request):
         all_services = models.Service.objects.all()
-        serialize_all_services = serializers.Service(all_services, many = True)
+        serialize_all_services = serializers.ServiceSerializer(all_services, many = True)
         return Response({
             'status':status.HTTP_200_OK,
             'all_services':serialize_all_services.data

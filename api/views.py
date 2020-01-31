187c6369be748350242c3f03a1fd9a5c33aa05eb
lwairore <kwairore@gmail.com>
2020-01-31 14:07:44 +0300

    Remove render import. Refactor importations of models as 'from . import models'. Refactor importations of serializers as 'from .serializers import serializers'. Refactor importation of HTTP_200_OK as 'from rest_framework import status'. Import IsAuthenticatedOrReadOnly from 'rest_framework.permissions'.

--- api/views.py
+++ api/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import NavBarItems, Service, Doctor
-from api.serializers.serializers import NavBarItemsSerializer, ServicesSerializer
+from . import models
+from .serializers import serializers
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 class NavBarItemsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset =  NavBarItems.objects.all()
     serializer_class = NavBarItemsSerializer
+
+
 class ServicesViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServicesSerializer

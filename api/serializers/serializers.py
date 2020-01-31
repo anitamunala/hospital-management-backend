d8c97717904b9a557fd0ad37aea0b656265ff0b2
lwairore <kwairore@gmail.com>
2020-01-31 16:10:16 +0300

    Import User model from "django.contrib.auth.models".
    Create UserSerializer class that extends to ModelSerializer. Create class Meta. Set model to User and fields to username.
    
    Create DoctorSerializer that extends to ModelSerializer. Create class Meta. Set model to Doctor and fields list to : 'name, email_address, gender, profile_picture, specialization, phone_number, user and doctors_id.

--- api/serializers/serializers.py
+++ api/serializers/serializers.py
@@ -1,12 +1,25 @@
-from api.models import NavBarItems, Service, Doctor
+from .. import models 
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class NavBarItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = NavBarItems
+        model = models.NavBarItems
         fields = ['name']
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Service
+        model = models.Service
         fields = ['name','description','image']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username',]
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True, many = False)
+    class Meta:
+        model =  models.Doctor
+        fields = ['name','email_address','gender','profile_picture','specialization','phone_number','user','doctors_id']

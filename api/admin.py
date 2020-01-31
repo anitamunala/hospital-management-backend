5bfbb2067b10f5d8824adef2d5c37c6ab4b8eed4
lwairore <kwairore@gmail.com>
2020-01-31 13:29:42 +0300

    overwrite site_header default value to 'SHIS ADMIN'

--- api/admin.py
+++ api/admin.py
@@ -7,3 +7,5 @@ admin.site.register(models.Doctor)
 admin.site.register(models.Specialist)
 admin.site.register(models.Patient)
 admin.site.register(models.Service)
+
+admin.site.site_header = "SHIS ADMIN"
